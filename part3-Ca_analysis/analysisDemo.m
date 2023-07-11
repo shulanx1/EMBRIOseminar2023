a4b0c1eaa6112411de4597f47163213266be665f
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-07-10 23:58:28 -0400

    Update analysisDemo.m

--- part3-Ca_analysis/analysisDemo.m
+++ part3-Ca_analysis/analysisDemo.m
@@ -2,35 +2,40 @@
 %% (1) Setting up the environment 
 set(0,'DefaultFigureWindowStyle','docked')
 format compact;
-addpath(genpath('main'));
-addpath(genpath('Pipelines'))
-addpath(genpath('Plotting'))
-params.caFr = 30.48;
-params.ts = 1/params.caFr:1/params.caFr:size(DeltaFoverF,2)/params.caFr;
+addpath(genpath('scripts'));
 %% (2) Visualize Segmented Data
 figure('Name','Calcium Segmentation'),
 plot_contours(A,C,ops,1);
-
 %% (3) Visualize Calcium Transience
-figure('Name','Single Calcium Transience'), plot(DeltaFoverF(115,:)); % Single trace
-figure('Name','Population Transience'), stack_plot(DeltaFoverF,1,5,1); % All traces
-figure,imagesc(DeltaFoverF),axis xy, colormap(hot)
+figure('Name','Single Calcium Transience'), plot(DeltaFoverF(1,:)); % Single trace
+figure('Name','Population Transience'), stack_plot(DeltaFoverF,1,5,0); % All traces
+figure('Name','Population Heatmap'),imagesc(DeltaFoverF),axis xy, colormap(hot),caxis([0 5])
 %% Spike detection from dF/F
 std_threshold = 3;     
 static_threshold = .01;
 Spikes = rasterizeDFoF(DeltaFoverF,std_threshold,static_threshold);
 figure('Name','Spiking Raster');Show_Spikes(Spikes);xlim([400 500])
-%% define co-active frames
-popCoactivity = sum(Spikes,1)/size(Spikes,1);
-[coactive_cells,detected_spikes] = coactive_index(Spikes,5000);
+%% Calculate coactivity across binary matrix (2 different ways)
+popCoactivity = sum(Spikes,1)/size(Spikes,1); % No binning
+[coactivityIndex,detectedSpikes] = coactive_index(Spikes,ceil(size(Spikes,2)/2)); %With binning
+figure('Name','Coactivity (no binning)'),bar(popCoactivity)
+figure('Name', 'Coactivity (binning)'),bar(coactivityIndex)
 %% Compute Sorenson-dice Correlation
 pks = 1;
-col = find(sum(Spikes,1)>pks);
+col = find(sum(Spikes(:,1:2000,1))>pks); % To save computation time
 coSpikes = Spikes(:,col);
 corr = correlation_dice(coSpikes);
+figure,imagesc(corr),colormap("jet");caxis([0 1]),colorbar
+% Plot non-zero correlation 
+figure,histogram(corr(corr>0),'Normalization','probability'),xlabel('Similar`ity Index'),ylabel('Probability')
 %% (4) Representing the data
 % Mean similarity
 % Connectivity
 % Event related calcium fluorescence
-figure,imagesc(corr),colormap("hot");
+Connected_ROI = Connectivity_dice(corr);
+figure('Name','Network Map'); NodeSize = 5;EdgeSize = 1;Cell_Map_Dice(AverageImage,Connected_ROI,ROIcentroid,NodeSize,EdgeSize)
+%% (5) Graph Theory Analysis
+[NumActiveNodes,NodeList,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,...
+    ActivityCentroid,ActivityCentroidVariance]...
+    = Network_Analysis(ROIcentroid,Connected_ROI);
 
